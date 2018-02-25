325c8b5f4abc2c5112f9986b9309901521b7a9ad
AlexRobertoCorrea <alx.rcorrea@gmail.com>
2018-02-25 16:21:56 -0300

    Card of bundles improved with more information

--- client/src/components/broadbandCard/BroadbandCard.js
+++ client/src/components/broadbandCard/BroadbandCard.js
@@ -5,8 +5,10 @@ class BroadbandCard extends Component {
   render() {
     return (
       <div>
+        <h3>Bundle</h3>
         <p>{Array.isArray(this.props.name) ? this.props.name.join(' + ') : this.props.name}</p>
-        <p>{this.props.totalPrice}</p>
+        <h3>Preço</h3>
+        <p>R$ {this.props.totalPrice},00</p>
         <Button variant="raised" color="primary">
           Comprar
         </Button>
